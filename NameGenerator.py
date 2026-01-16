6193af7a7b9abc7f243bad9b4161f3bf276b3273
keithkida <kjkida@ucsc.edu>
2026-01-16 03:58:06 +0000

    test name for Elagant and Whimsy

--- NameGenerator.py
+++ NameGenerator.py
@@ -16,3 +16,5 @@ def tonegenerator(tone):
     return random.choice(prefix) + " " + random.choice(name)
 
 print(tonegenerator("Harsh"))
+print(tonegenerator("Elagant"))
+print(tonegenerator("Whimsy"))
