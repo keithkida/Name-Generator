98fa5f93e6f886eca89e4b639d8f611a06d09747
keithkida <kjkida@ucsc.edu>
2026-01-16 03:43:50 +0000

    tone generator for now

--- NameGenerator.py
+++ NameGenerator.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+def tonegenerator(tone): 
+    if tone == "harsh":
+        prefix = ["Brutal", "Ultimate", "Vengful"]
+        name = name = ["Bulls", "Tigers", "Warriors", "Marauders", "Ravagers", "Ironclaws"] 
+    elif tone == "Elagant": 
+        prefix = ["Shining", "Victorious", "Hopeful"]
+        name = ["Saints", "Sanctum", "Concord", "Order", "Assembly", "Chronicle"]
+    elif tone == "Whimsy": 
+        prefix = ["Wacky", "Playful", "Wonky"]
+        name = ["Tricksters", "Mischiefs", "Gigglefolk", "Bumblekins", "Jesters", "Jokers"]
