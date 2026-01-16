72711b3c4b79f10556ba3fdac4407badf29d65e7
keithkida <kjkida@ucsc.edu>
2026-01-16 04:00:39 +0000

    new tones of Mtstical and Futuristic

--- NameGenerator.py
+++ NameGenerator.py
@@ -6,10 +6,16 @@ def tonegenerator(tone):
         name = ["Bulls", "Tigers", "Warriors", "Marauders", "Ravagers", "Ironclaws"] 
     elif tone == "Elagant": 
         prefix = ["Shining", "Victorious", "Hopeful", "Noble", "Radiant", "Glorious"]
-        name = ["Saints", "Sanctum", "Concord", "Order", "Assembly", "Chronicle"]
+        name = [, "Sanctum", "Concord", "Order", "Assembly", "Chronicle"]
     elif tone == "Whimsy": 
         prefix = ["Wacky", "Playful", "Wonky", "Zany", "Quirky", "Bubbly"]
         name = ["Tricksters", "Mischiefs", "Gigglefolk", "Bumblekins", "Jesters", "Jokers"]
+    elif tone == "Mystical":
+        prefix = ["Enchanted", "Arcane", "Mystic", "Ethereal", "Celestial", "Otherworldly"]
+        name = ["Wizards", "Sorcerers", "Mystics", "Seers", "Oracles", "Spellbinders"]
+    elif tone == "Futuristic":
+        prefix = ["Cyber", "Neo", "Quantum", "Hyper", "Nano", "Techno"]
+        name = ["Runners", "Synths", "Drones", "Cyborgs", "Mechs", "Bots"]
     else:
         return "Invalid Choice"
 
